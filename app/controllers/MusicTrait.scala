2ec24fa104764d577fbda92f97fe1fa923b393ab
wg <williamgelhar@gmail.com>
2013-08-09 04:00:21 -0400

    Implemented a basic trait that holds the functionality to look up music information from the last.fm api

---
+++ app/controllers/MusicTrait.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import play.api.libs.ws._
+import play.api.libs.ws.WS
+import play.api.libs.json
+
+import play.api.libs.concurrent.Execution.Implicits._
+
+/* Provides an easy way of checking for the user being logged in across the controllers.
+ * Controllers that require user authentication use this trait and wrap the necessary action with isAuthenticated
+ * to implement user authentication handling.
+ */
+trait MusicSearch {
+
+	private val last_fm_url = "http://ws.audioscrobbler.com/2.0/"
+
+	private def lfm_track_search_url(query: String): String = last_fm_url + 
+		"?method=track.search" + 
+		"&track=" + query + 
+		"&api_key=" + System.getenv("LASTFM_API_KEY") + 
+		"&format=" + "json"
+
+	def searchSong(track: String) = {
+		/* TODO: actually do this query and then serve the results */
+		WS.url( lfm_track_search_url(track) ).get()
+	}
+}
