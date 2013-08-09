5df2aa76c2ada154ce4ca62627fe500eeafa06ca
wg <williamgelhar@gmail.com>
2013-08-09 04:01:15 -0400

    Implemented the basic steps for the user to look up information on music from the site.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,26 @@
 package controllers
 
 import play.api._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
 
-object Application extends Controller {
+import play.api.libs.concurrent.Execution.Implicits._
+
+object Application extends Controller with MusicSearch {
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def search = Action { implicit request =>
+  		Async {
+  			searchSong( Form("query" -> text).bindFromRequest.get.replace(" ","+") ).map { response => Ok(response.json) }
+  		}
+  }  
+
+  def  submitSong = Action { implicit request => 
+  	val (artist, title, mbid) = Form(tuple("artist" -> text, "title" -> text, "mbid" -> text)).bindFromRequest.get
+  	Ok(artist + ", " + title + ", " + mbid)
+  }
 }
