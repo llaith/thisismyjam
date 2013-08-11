820b85e399b8505000822d2a54fd83d3f7e111c8
wg <williamgelhar@gmail.com>
2013-08-11 04:46:27 -0400

    Implemented the usage of the MusicModel model in the main application controller so it interacts with the mongo database.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,8 +4,9 @@ import play.api._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
-
 import play.api.libs.concurrent.Execution.Implicits._
+
+import models.MusicModel
 
 object Application extends Controller with MusicSearch {
   
@@ -13,14 +14,20 @@ object Application extends Controller with MusicSearch {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def search = Action { implicit request =>
-  		Async {
-  			searchSong( Form("query" -> text).bindFromRequest.get.replace(" ","+") ).map { response => Ok(response.json) }
-  		}
-  }  
+    def search = Action { implicit request =>
+        Async {
+    		musicSearch( 
+                Form("query" -> text).bindFromRequest.get.replace(" ","+") 
+            ).map { response => Ok(response.json) }
+        }
+    }  
 
-  def  submitSong = Action { implicit request => 
-  	val (artist, title, mbid) = Form(tuple("artist" -> text, "title" -> text, "mbid" -> text)).bindFromRequest.get
-  	Ok(artist + ", " + title + ", " + mbid)
-  }
+    def submitSong = Action { implicit request => 
+        val (artist, title, mbid) = Form(tuple("artist" -> text, "title" -> text, "mbid" -> text)).bindFromRequest.get
+        Async {
+            MusicModel.submitSong(artist, title, mbid, "last.fm").map(result => Ok(result))
+         }
+    }
+
+    //Ok(artist + ", " + title + ", " + mbid)
 }
