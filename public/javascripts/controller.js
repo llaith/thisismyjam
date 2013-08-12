89ce6a7393d77c553a307588debbe38f0ddd00a1
wg <williamgelhar@gmail.com>
2013-08-11 23:21:47 -0400

    Implemented a client-side limiting of results for searches.

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -5,17 +5,28 @@
 function jamsControl($scope, $http) {
 
 	$http.get('/songs/list').success(function(data) {
-		console.log(data);
 		$scope.jams = data;
 	});
 }
 
 function resultsControl($scope, $http) {
 
+    //number of results to show, starts at 5, is a variable so we can show more
+    $scope.maxResults = 5;
+    //visibility of the show more results button, only toggled when a search is performed
+    $scope.showMoreResultsButton = false;
+
+    $scope.moreResults = function() {
+        $scope.maxResults += 5;
+        if ($scope.maxResults >= 15){
+          $scope.showMoreResultsButton = false;
+        }
+    };
+
   	$scope.search = function(){
   		$http.get('/search', {'params': {"query" : $scope.search_query} }).success( function(data) {
   			$scope.tracks = data.results.trackmatches.track;
-  			console.log(data);
+  			$scope.showMoreResultsButton = true;
   		});
   	};
 }
