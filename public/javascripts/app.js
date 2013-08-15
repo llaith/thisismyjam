d8f74d1721324b98f5f9775e8611bd446b3281d0
wg <williamgelhar@gmail.com>
2013-08-15 18:30:44 -0400

    Moved the Angular controllers to the Angular module for the site.
    This is a better idea for Angular reasons even though 90% of their documentation and examples just throw controllers into the global scope, relegating this good behaviour to a small note in the developer's guide for controllers.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,45 @@
-angular.module('myjam', ['appMessenger', 'loadingIndicator']);
+var myjamModule = angular.module('myjam', ['appMessenger', 'loadingIndicator']).controller();
+
+/* Controllers */
+
+myjamModule.controller('jamsControl', ['$scope', '$http', function($scope, $http) {
+	'use strict';
+
+	$http.get('/songs/list', {'loadingItemID' : 'list'}).
+    success(function(data) {
+  		$scope.jams = data;
+  	});
+}]);
+
+myjamModule.controller('resultsControl', ['$scope', '$http', function($scope, $http) {
+	'use strict';
+
+    //number of results to show, starts at 5, is a variable so we can show more
+    $scope.maxResults = 5;
+    //visibility of the show more results button, only toggled when a search is performed
+    $scope.showMoreResultsButton = false;
+
+    $scope.moreResults = function() {
+        $scope.maxResults += 5;
+        if ($scope.maxResults >= 15){
+          $scope.showMoreResultsButton = false;
+        }
+    };
+
+  	$scope.search = function(){
+  		$http.get('/search', 
+        {'params': {"query" : $scope.search_query},
+         'loadingItemID' : 'search' }
+      ).success( function(data) {
+        
+  			$scope.tracks = data.results.trackmatches.track;
+  			$scope.showMoreResultsButton = true;
+  		});
+  	};
+
+    $scope.submit = function(artist, track, mbid) {
+      $http.post('/jam/song', {"artist": artist, "track": track, "mbid": mbid});
+      $scope.tracks = null;
+      $scope.showMoreResultsButton = false;
+    };
+}]);
